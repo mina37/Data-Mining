3146efcac3e15eec6150d79cf005b1507e4a2bcc
Mina Hanna <Mina Hanna>
2016-05-08 14:51:35 +0200

    random generator + years

--- random.h
+++ random.h
@@ -6,17 +6,19 @@ using namespace std;
 
 int random()
 {
+    const int base = 10;
+    const int div  = 10;
 	srand(time(NULL));
 	fstream f;
 	f.open("randinputs.txt");
 	for (int year = 2009;year <= 2016;year++)
 	{
-		//f << year << endl;
-		for (int customers = 0; customers <= (rand() % 10 + 10) ;customers++)
+		f << year << endl;
+		for (int customers = 0; customers <= (rand() % div + base) ;customers++)
 		{
 			f << rand() % 2 + 10 << " ";
 			f << rand() % 4 + 20 << " ";
-			for (int products = 0; products <= (rand() % 10 + 5);products++)
+			for (int products = 0; products <= (rand() % div + base);products++)
 				f << rand() % 7 + (rand() % 3 + 1) * 100 << " ";
 			f << "-1" << endl;
 		}
